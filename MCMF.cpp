2f73be427c9c2c6a5216163c0a9d76511cfc267e
Seojin Kim <rlatjwls7882@gmail.com>
2025-04-17 09:42:34 +0900

    Create MCMF.cpp

---
+++ MCMF.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int INF = 0x3f3f3f3f;
+const int MAX = 2502;
+const int S = MAX-2, E = MAX-1;
+int c[MAX][MAX], f[MAX][MAX], cost[MAX][MAX], curCost[MAX], prv[MAX];
+bool inQueue[MAX];
+vector<vector<int>> conn(MAX);
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+
+    int totalCost=0;
+    while(true) {
+        memset(prv, -1, sizeof prv);
+        memset(inQueue, 0, sizeof inQueue);
+        fill(curCost, curCost+MAX, INF);
+        curCost[S]=0;
+        queue<int> q; q.push(S);
+        inQueue[S]=true;
+        while(!q.empty()) {
+            int cur = q.front(); q.pop();
+            inQueue[cur]=false;
+            for(int next:conn[cur]) {
+                if(c[cur][next]-f[cur][next]>0 && curCost[next]>curCost[cur]+cost[cur][next]) {
+                    curCost[next] = curCost[cur] + cost[cur][next];
+                    prv[next] = cur;
+                    if(!inQueue[next]) {
+                        q.push(next);
+                        inQueue[next]=true;
+                    }
+                }
+            }
+        }
+        if(prv[E]==-1) break;
+
+        int flow = INF;
+        for(int i=E;i!=S;i=prv[i]) {
+            flow = min(flow, c[prv[i]][i]-f[prv[i]][i]);
+        }
+        for(int i=E;i!=S;i=prv[i]) {
+            f[prv[i]][i] += flow;
+            f[i][prv[i]] -= flow;
+            totalCost += cost[prv[i]][i]*flow;
+        }
+    }
+    cout << totalCost;
+}
