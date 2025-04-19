76a24c53af8682741619435129b10beb3401fce3
Seojin Kim <rlatjwls7882@gmail.com>
2025-04-19 18:15:55 +0900

    Update Segment-Tree.cpp

--- Segment-Tree.cpp
+++ Segment-Tree.cpp
@@ -2,11 +2,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+/**
+ * MAX : 배열 크기
+ * _size : 사용할 배열의 크기 (_size/2 ~ _size-1까지 값이 저장됨)
+ * arr : 배열
+ */
 typedef long long ll;
-const int MAX = 3'000'001;
+const int MAX = 3'000'000;
 int _size=1;
 ll arr[MAX];
 
+/**
+ * 세그먼트 트리에서 값 꺼내기 (이 문제에선 구간합)
+ */
 ll sum(int L, int R, int nodeNum, int nodeL, int nodeR) {
     if(R<nodeL || nodeR<L) return 0;
     if(L<=nodeL && nodeR<=R) return arr[nodeNum];
@@ -14,6 +22,9 @@ ll sum(int L, int R, int nodeNum, int nodeL, int nodeR) {
     return sum(L, R, nodeNum*2, nodeL, mid) + sum(L, R, nodeNum*2+1, mid+1, nodeR);
 }
 
+/**
+ * 세그먼트 트리에 값 넣기
+ */
 void update(int nodeNum, ll val) {
     nodeNum += _size/2;
     arr[nodeNum]=val;
@@ -26,9 +37,9 @@ void update(int nodeNum, ll val) {
 void init(int N) {
     while(_size<N) _size<<=1;
     _size<<=1;
-    
+
     for(int i=_size/2;i<_size/2+N;i++) cin >> arr[i];
-    
+
     for(int nodeNum=_size/2-1;nodeNum>0;nodeNum--) {
         arr[nodeNum]=arr[nodeNum*2]+arr[nodeNum*2+1];
     }
