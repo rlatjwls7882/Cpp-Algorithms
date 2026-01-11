7fa4884bb440e686767a4ce4460880f8721ffebc
Seojin Kim <rlatjwls7882@gmail.com>
2026-01-11 15:59:37 +0900

    MCMF

--- template/Flow.cpp
+++ template/Flow.cpp
@@ -1,21 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
+typedef long long ll;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3f;
 
 struct Dinic { // O(V²E), 모든 간선의 용량이 1이면 O(min(V^(2/3), E^(1/2))E)
-    typedef long long ll;
-    const int INF = 0x3f3f3f3f;
-    const ll LINF = 0x3f3f3f3f3f3f3f3f;
     struct Edge {
         int to, rev;
         ll c;
     };
-    int S=0, E;
+    int S, E;
     vector<int> level, work;
     vector<vector<Edge>> conn;
 
     inline Dinic(int SZ, int s, int e) {
-        S=s;
-        E=e;
+        S=s; E=e;
         work = vector<int>(SZ);
         level = vector<int>(SZ);
         conn = vector<vector<Edge>>(SZ);
@@ -65,5 +64,68 @@ struct Dinic { // O(V²E), 모든 간선의 용량이 1이면 O(min(V^(2/3), E^(
             while(flow=dfs(S, LINF)) total+=flow;
         }
         return total;
+    }
+};
+
+struct MCMF { // O(FVE) F는 최대유량
+    int S, E;
+    vector<int> prv;
+    vector<bool> inQ;
+    vector<ll> curCost;
+    vector<vector<int>> conn;
+    vector<vector<ll>> c, cost;
+
+    inline MCMF(int SZ, int s, int e) {
+        S=s; E=e;
+        prv = vector<int>(SZ);
+        inQ = vector<bool>(SZ);
+        curCost = vector<ll>(SZ);
+        conn = vector<vector<int>>(SZ);
+        c = vector<vector<ll>>(SZ, vector<ll>(SZ));
+        cost = vector<vector<ll>>(SZ, vector<ll>(SZ));
+    }
+
+    inline void edge(int u, int v, ll cap, ll co) {
+        c[u][v]=cap;
+        cost[u][v]=co;
+        cost[v][u]=-co;
+        conn[u].push_back(v);
+        conn[v].push_back(u);
+    }
+
+    inline pair<ll, ll> res() {
+        ll totalFlow=0, totalCost=0;
+        while(true) {
+            queue<int> q; q.push(S);
+            fill(prv.begin(), prv.end(), -1);
+            fill(curCost.begin()+1, curCost.end(), LINF);
+            while(!q.empty()) {
+                int cur = q.front(); q.pop();
+                inQ[cur]=false;
+                for(int next:conn[cur]) {
+                    if(c[cur][next] && curCost[next]>curCost[cur]+cost[cur][next]) {
+                        curCost[next]=curCost[cur]+cost[cur][next];
+                        prv[next]=cur;
+                        if(!inQ[next]) {
+                            q.push(next);
+                            inQ[next]=true;
+                        }
+                    }
+                }
+            }
+            if(prv[E]==-1) break;
+
+            ll flow=LINF;
+            for(int i=E;i!=S;i=prv[i]) {
+                flow = min(flow, c[prv[i]][i]);
+            }
+            for(int i=E;i!=S;i=prv[i]) {
+                c[prv[i]][i]-=flow;
+                c[i][prv[i]]+=flow;
+            }
+            totalFlow+=flow;
+            totalCost+=curCost[E]*flow;
+        }
+        return {totalFlow, totalCost};
     }
 };
