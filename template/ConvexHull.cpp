b3316349ef35868304bba06f5ca548c235c41487
Seojin Kim <rlatjwls7882@gmail.com>
2026-01-11 12:58:54 +0900

    다각형의 넓이, 각도 정렬

--- template/ConvexHull.cpp
+++ template/ConvexHull.cpp
@@ -5,12 +5,13 @@ struct ConvexHull {
     using ll = long long;
     struct point {
         ll x, y, p=0, q=0;
-        bool operator<(const point o) const {
+        bool operator<(const point &o) const {
             if(p*o.q!=o.p*q) return p*o.q>o.p*q;
+            if(p*p+q*q!=o.p*o.p+o.q*o.q) return p*p+q*q<o.p*o.p+o.q*o.q;
             if(y!=o.y) return y<o.y;
             return x<o.x;
         }
-        bool operator==(const point o) const {
+        bool operator==(const point &o) const {
             return x==o.x && y==o.y;
         }
     };
@@ -36,7 +37,28 @@ struct ConvexHull {
         return a.x*b.y+b.x*c.y+c.x*a.y-a.x*c.y-b.x*a.y-c.x*b.y;
     }
 
-    inline ConvexHull(vector<point> v) {
+    inline ll area() { // area*2
+        ll ret=0;
+        for(int i=1;i<hull.size()-1;i++) ret += triArea(hull[0], hull[i], hull[i+1]);
+        return ret;
+    }
+
+    inline void angleSort(vector<point> &v) {
+        for(int i=0;i<v.size();i++) v[i].p=v[i].q=0;
+        sort(v.begin(), v.end());
+
+        for(int i=1;i<v.size();i++) {
+            v[i].p = v[i].x-v[0].x;
+            v[i].q = v[i].y-v[0].y;
+        }
+        sort(v.begin(), v.end());
+
+        int revIdx=v.size()-1;
+        while(revIdx>1 && ccw(v[0], v[revIdx-1], v[revIdx])==0) revIdx--;
+        reverse(v.begin()+revIdx, v.end());
+    }
+
+    inline ConvexHull(vector<point> &v) {
         for(int i=0;i<v.size();i++) v[i].p=v[i].q=0;
         sort(v.begin(), v.end());
 
@@ -53,7 +75,7 @@ struct ConvexHull {
         }
     }
 
-    inline bool inConvexHull(point p) {
+    inline bool inHull(point &p) {
         if(ccw(hull[0], hull[1], p)<0) return false;
         if(ccw(hull[0], hull[hull.size()-1], p)>0) return false;
 
@@ -66,7 +88,7 @@ struct ConvexHull {
         return ccw(hull[l], hull[l+1], p)>=0;
     }
 
-    inline ll rotating_calipers() {
+    inline ll rotatingCalipers() {
         ll ret=0, i=0, j=1;
         while(i<hull.size() && j<hull.size()) {
             ret = max(ret, dist(hull[i], hull[j]));
